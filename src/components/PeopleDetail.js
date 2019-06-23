38f1eba177bcdb609f080c10c9be8000eb63493b
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 21:30:25 -0400

    17 more on detail view

--- src/components/PeopleDetail.js
+++ src/components/PeopleDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Text, Image } from 'react-native';
+import { View, StyleSheet, ScrollView, Text, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import EvilIcon from 'react-native-vector-icons/EvilIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
@@ -33,7 +33,47 @@ class PeopleDetail extends Component {
                      onPress={() => this.props.noneSelected()}/>
                     <Text style={[theme.cardTitleStyle, styles.title1]}>{this.props.person.firstName} {this.props.person.LastName}</Text>
                     <Text style={[theme.cardTitleStyle, styles.title2]}>from {this.props.person.company}</Text>
-
+                    <View style={styles.textArea}>
+                        <MaterialIcon name={'phone'} size={40} style={styles.textIcons}/>
+                        <Text style={theme.cardContentStyle}>{this.props.person.phone}</Text>
+                    </View>
+                    <View style={styles.textArea}>
+                        <MaterialIcon name={'email'} size={40} style={styles.textIcons}/>
+                        <Text style={theme.cardContentStyle}>{this.props.person.email}</Text>
+                    </View>
+                    <View style={styles.textArea}>
+                        <MaterialIcon name={'assignment'} size={40} style={styles.textIcons}/>
+                        <Text style={theme.cardContentStyle}>{this.props.person.project}</Text>
+                    </View>
+                    <View style={styles.textArea}>
+                        <MaterialIcon name={'mode-edit'} size={40} style={styles.textIcons}/>
+                        <Text style={theme.cardContentStyle}>{this.props.person.notes}</Text>
+                    </View>
+                    <View>
+                        <TouchableOpacity>
+                            <Image 
+                                source={require('../images/call@2x.png.png')}
+                                style={styles.actionImage} 
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image 
+                                source={require('../images/email@2x.png.png')}
+                                style={styles.actionImage} 
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image 
+                                source={require('../images/sms@2x.png.png')}
+                                style={styles.actionImage} 
+                            />
+                        </TouchableOpacity>
+                        <View style={styles.actionArea}>
+                            <text>Call</text>
+                            <text>Email</text>
+                            <text>SMS</text>
+                        </View>
+                    </View>
                     </ScrollView>
             </View>
         )
