04b43f926faf20b1e9f7874ded92774d36ea466e
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 22:25:36 -0400

    19 item to list

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import Icon from 'react-native-vector-icons/EvilIcons';
 import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/EvilIcons';
 import PeopleItem from './PeopleItem';
+import PeopleDetail from './PeopleDetail';
 
 const styles = StyleSheet.create({
     container: {
@@ -20,20 +21,35 @@ class PeopleList extends Component {
             <Icon name={'user'} size={50} color={tintColor} />
         )
     }
+
+    renderInitialView() {
+        if (this.props.detailView === true) {
+            return (
+                <PeopleDetail />
+            )
+        } else {
+            return (
+                <FlatList
+                    data={this.props.people}
+                    renderItem={({item}) => <PeopleItem people={item} />}
+                />
+            )
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
-            <FlatList
-                data={this.props.people}
-                renderItem={({item}) => <PeopleItem people={item} />}
-            />
+                {this.renderInitialView()}
             </View>
         )
     }
 }
 
 const mapStateToProps = state => {
-    return { people: state.people }
+    return { 
+        people: state.people,
+        detailView: state.detailView, 
+    }
 }
 
 export default connect(mapStateToProps)(PeopleList);
