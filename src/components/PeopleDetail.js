0dd38c467a6d6345969acb8c629cf8c53ae4d668
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 21:18:27 -0400

    16 the scroll on detail

--- src/components/PeopleDetail.js
+++ src/components/PeopleDetail.js
@@ -4,7 +4,10 @@ import { connect } from 'react-redux';
 import EvilIcon from 'react-native-vector-icons/EvilIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import SimpleIcon from 'react-native-vector-icons/SimpleLineIcons';
+import { getTheme } from 'react-native-material-kit';
 import * as actions from '../actions';
+
+const theme = getTheme();
 
 const styles = StyleSheet.create({
     container: {
@@ -20,7 +23,18 @@ class PeopleDetail extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <ScrollView></ScrollView>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <Image 
+                        source={require('../images/background.jpg')}
+                        style={[theme.cardImageStyle, styles.image]}
+                    />
+                    <EvilIcon name={'user'} size={100} style={styles.icon} />
+                    <SimpleIcon name={'close'} size={30} style={styles.closeIcon}
+                     onPress={() => this.props.noneSelected()}/>
+                    <Text style={[theme.cardTitleStyle, styles.title1]}>{this.props.person.firstName} {this.props.person.LastName}</Text>
+                    <Text style={[theme.cardTitleStyle, styles.title2]}>from {this.props.person.company}</Text>
+
+                    </ScrollView>
             </View>
         )
     }
